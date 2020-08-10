c5cf714200c4c03dd9da1d958052f1a244d26412
Jarred Baca <jarredbaca@MacBook-Pro.local>
2020-08-09 23:12:49 -0600

    finsihed user routes

---
+++ models/index.js
@@ -0,0 +1,36 @@
+const User = require('./User');
+const Post = require("./Post");
+const Comment = require('./Comment');
+
+//associations
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    foreignKeyConstraint: true, 
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    foreignKeyConstraint: true, 
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    foreignKeyConstraint: true, 
+    onDelete: 'cascade' 
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    foreignKeyConstraint: true, 
+    onDelete: 'cascade' 
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id', 
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+});
+
+module.exports = { User, Post, Comment};
